beec1b266d4d0c5a4812c3a7e0ee330a0e963176
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-22 14:20:55 +0430

    add equals and hashcode in utility

--- src/main/java/com/example/Utility.java
+++ src/main/java/com/example/Utility.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Objects;
+
 public class Utility {
     private double sellerUtility;
     private double buyerUtility;
@@ -27,5 +29,17 @@ public class Utility {
         this.buyerUtility = buyerUtility;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Utility utility = (Utility) o;
+        return Double.compare(utility.sellerUtility, sellerUtility) == 0 &&
+                Double.compare(utility.buyerUtility, buyerUtility) == 0;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(sellerUtility, buyerUtility);
+    }
 }
