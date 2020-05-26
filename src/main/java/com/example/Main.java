89d6620c91ac33cb710eab5530d81bcc7535fd5e
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-26 17:42:49 +0430

    find pareto points with store pareto values in array idea

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,105 +7,69 @@ import org.knowm.xchart.XYChart;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * @author Ehsan Maddahi
+ */
 public class Main {
 
-
     public static void main(String[] args) {
+        // Initial pareto points array
+        Utility[] paretoPoints = new Utility[100000001];
 
-        /*List<Utility> sortedUtilityResult = mockInputVariables()
-                .stream()
-                .sorted((utility1, utility2) -> {
-                    if (utility1.getBuyerUtility() > utility2.getBuyerUtility()) {
-                        return 1;
-                    } else if (utility1.getBuyerUtility() == utility2.getBuyerUtility()) {
-                        return Double.compare(utility1.getSellerUtility(), utility2.getSellerUtility());
-                    } else {
-                        return -1;
-                    }
-                }).collect(Collectors.toList());*/
+        // Find pareto points of dataset
+        paretoValues(paretoPoints);
 
-            Set<Utility> allUtilities = mockInputVariables();
-//        Set<Utility> allUtilities = new HashSet<>();
-//
-//        allUtilities.add(new Utility(0.1, 1.0));
-//        allUtilities.add(new Utility(0.17, 1.0));
-//        allUtilities.add(new Utility(0.19, 0.81));
-//        allUtilities.add(new Utility(0.24, 0.81));
-//        allUtilities.add(new Utility(0.30, 0.81));
-//        allUtilities.add(new Utility(0.30, 0.82));
-//        allUtilities.add(new Utility(0.30, 0.82));
-//        allUtilities.add(new Utility(0.33, 0.63));
-//        allUtilities.add(new Utility(0.46, 0.63));
-//        allUtilities.add(new Utility(0.53, 0.63));
-//        allUtilities.add(new Utility(0.55, 0.44));
-//        allUtilities.add(new Utility(0.69, 0.44));
-//        allUtilities.add(new Utility(0.75, 0.44));
-//        allUtilities.add(new Utility(0.78, 0.25));
-//        allUtilities.add(new Utility(0.91, 0.33));
-//        allUtilities.add(new Utility(0.98, 0.33));
-//        allUtilities.add(new Utility(1.0, 0.14));
+        // Get x values of pareto utilities
+        List<Double> x = Arrays
+                .stream(paretoPoints)
+                .filter(Objects::nonNull)
+                .map(Utility::getSellerUtility)
+                .collect(Collectors.toList());
 
-        List<Utility> paretoPoints = findParetoPoints(allUtilities).stream()
-                .sorted((utility1, utility2) -> {
-                    if (utility1.getBuyerUtility() > utility2.getBuyerUtility()) {
-                        return 1;
-                    } else if (utility1.getBuyerUtility() == utility2.getBuyerUtility()) {
-                        return Double.compare(utility1.getSellerUtility(), utility2.getSellerUtility());
-                    } else {
-                        return -1;
-                    }
-                }).collect(Collectors.toList());
+        // Get y values og pareto utilities
+        List<Double> y = Arrays
+                .stream(paretoPoints)
+                .filter(Objects::nonNull)
+                .map(Utility::getBuyerUtility)
+                .collect(Collectors.toList());
 
-        List<Double> sellerList = paretoPoints.stream().map(Utility::getSellerUtility).collect(Collectors.toList());
-        List<Double> buyerList = paretoPoints.stream().map(Utility::getBuyerUtility).collect(Collectors.toList());
 
-        // draw chart
-        XYChart chart = QuickChart.getChart("Pareto Chart", "Seller", "Buyer",
-                "pareto line", sellerList, buyerList);
+        // Draw Pareto Curve
+        XYChart chart = QuickChart.getChart("Pareto Chart", "Seller", "Buyer", "pareto curve", x, y);
 
         new SwingWrapper(chart).displayChart();
     }
 
-    private static Set<Utility> findParetoPoints(Set<Utility> allUtilities) {
-        Set<Utility> paretoPoints = new HashSet<>();
+    /**
+     * Check New point is pareto and store in array or not.
+     *
+     * @param paretoPoints array of pareto points
+     * @param newUtility new point
+     */
+    private static void checkIsPareto(Utility[] paretoPoints, Utility newUtility) {
 
-        for (Utility currentUtility: allUtilities) {
-            boolean isCurrentUtilityPareto = true;
-            // check current utility greater that other utilities in fixed buyer values
-            for (Utility comparedUtility : allUtilities) {
-                if (Double.compare(currentUtility.getBuyerUtility(), comparedUtility.getBuyerUtility()) == 0 &&
-                    Double.compare(currentUtility.getSellerUtility(), comparedUtility.getSellerUtility()) < 0) {
-                    isCurrentUtilityPareto = false;
-                    break;
-                }
-            }
-            // if current utility was greater than other utilities in fixed buyer values
-            if (isCurrentUtilityPareto) {
-                // check current utility greater that other utilities in fixed seller values
-                for (Utility comparedUtility : allUtilities) {
-                    if (Double.compare(currentUtility.getSellerUtility(), comparedUtility.getSellerUtility()) == 0 &&
-                        Double.compare(currentUtility.getBuyerUtility(), comparedUtility.getBuyerUtility()) < 0) {
-                        isCurrentUtilityPareto = false;
-                        break;
-                    }
-                }
-            }
-            // current item is pareto point
-            if (isCurrentUtilityPareto) {
-                paretoPoints.add(currentUtility);
+        int index = (int) (Math.floor(newUtility.getSellerUtility() * 100000000));
+        if (paretoPoints[index] == null || paretoPoints[index].getBuyerUtility() < newUtility.getBuyerUtility()) {
+            paretoPoints[index] = new Utility(newUtility.getSellerUtility(), newUtility.getBuyerUtility());
+        } else {
+            if (paretoPoints[index].getBuyerUtility() == newUtility.getBuyerUtility()) {
+                paretoPoints[index] =
+                        new Utility(Math.max(newUtility.getSellerUtility(), paretoPoints[index].getSellerUtility()),
+                                newUtility.getBuyerUtility());
             }
         }
-
-        return paretoPoints;
     }
 
+    /**
+     * Find pareto points
+     *
+     * @param paretoPoints
+     */
+    private static void paretoValues(Utility[] paretoPoints) {
 
-    private static Set<Utility> mockInputVariables() {
-        Set<Utility> utilities = new HashSet<>();
-
-        for (int price = 100; price <= 900; price+=300) {
-            for (int warranty = 0; warranty <= 36; warranty+=6) {
-                for (int sendDuration = 1; sendDuration <= 21; sendDuration+=4) {
+        for (int price = 100; price <= 900; price++) {
+            for (int warranty = 0; warranty <= 36; warranty++) {
+                for (int sendDuration = 1; sendDuration <= 21; sendDuration++) {
                     for (int returnProduct = 0; returnProduct <= 1; returnProduct++) {
                         double sellerUtilityResult = calculateUtility(price, 100, 900, 100) * 0.5
                                 + (calculateUtility(36, warranty, 36, 0) * 0.2)
@@ -117,15 +81,23 @@ public class Main {
                                 + calculateUtility(21, sendDuration, 21, 1) * 0.6
                                 + returnProduct * 0.2;
 
-                        utilities.add(new Utility(sellerUtilityResult, buyerUtilityResult));
+                        Utility newUtility = new Utility(sellerUtilityResult, buyerUtilityResult);
+                        checkIsPareto(paretoPoints, newUtility);
                     }
                 }
             }
         }
-
-        return utilities;
     }
 
+    /**
+     * Calculate utility point value
+     *
+     * @param first
+     * @param second
+     * @param max
+     * @param min
+     * @return utility value of current point
+     */
     private static double calculateUtility(double first, double second, double max, double min) {
         return (first - second) / (max - min);
     }
